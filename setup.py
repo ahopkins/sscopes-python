85c6a20e297142bb66c5b98090ededbb7a7e808a
Adam Hopkins <admhpkns@gmail.com>
2018-04-29 11:34:56 +0300

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='sscopes',
+    version='0.1dev',
+    author="Adam Hopkins",
+    author_email="admhpkns@gmail.com",
+    license='MIT',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3 :: Only',
+    ]
+)
